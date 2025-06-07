6a39072b52ebd74d94a38fff0fb80eb48db185a9
ahaoboy <504595380@qq.com>
2025-06-07 18:18:47 +0800

    add windows

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "not found target"
+    exit 1
+fi
+
+TARGET=$1
+
+git clone https://github.com/bellard/quickjs.git quickjs --depth=1
+cd quickjs
+
+make
+
+cp ./qjs ../dist/qjs
+cp ./qjsc ../dist/qjsc
+cp ./qjsc ../dist/qjsc
+cp -r ./libquickjs* ../dist
+
+cd ..
+
+ls -lh dist
+
+tar -czf ./qjs-${TARGET}.tar.gz -C dist .
+ls -l ./qjs-${TARGET}.tar.gz
+
+
+echo "console.log(1+1)" >> ./test.js
+./qjs ./test.js
